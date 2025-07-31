0da1cabce4f31e70e3c0fc26f97f99345d5ac99a
viccent55 <viccent55@gmail.com>
2025-07-31 13:07:05 +0700

    update api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE,
+      apiBase: 'http://api.szy7.com/apiv1',
     },
   },
 
