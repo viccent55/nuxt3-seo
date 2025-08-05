e8a17bb0705603a4ddcf309e3204d13fecf8f7e1
viccent55 <viccent55@gmail.com>
2025-08-05 23:41:15 +0700

    fixed improve Largest Contentful Paint (LCP)

--- components/ContentArticle.vue
+++ components/ContentArticle.vue
@@ -37,23 +37,27 @@
   // Watch for changes in the article detail
   watchEffect(async () => {
     loading.value = true;
-    if (props.content && typeof window !== 'undefined') {
+    if (props.content && typeof window !== "undefined") {
       const parser = new DOMParser();
       const doc = parser.parseFromString(props.content, "text/html");
       // Decrypt images
       const images = doc.querySelectorAll("img[data-lazy-src]");
-      for (const img of <any>images) {
-        const lazySrc = img.getAttribute("data-lazy-src");
-        if (lazySrc) {
-          try {
-            await decryptImage(lazySrc);
-            img.src = decryptedImage.value;
-          } catch (error) {
-            console.error("Error decrypting image:", error);
-          }
-        } else {
-          console.warn("Lazy source is undefined");
-        }
+      if (images && images.length) {
+        await Promise.all(
+          Array.from(images).map(async (img: EmptyObjectType) => {
+            const lazySrc = img.getAttribute("data-lazy-src");
+            if (lazySrc) {
+              try {
+                await decryptImage(lazySrc);
+                img.src = decryptedImage.value;
+              } catch (error) {
+                console.error("Error decrypting image:", error);
+              }
+            } else {
+              console.warn("Lazy source is undefined");
+            }
+          })
+        );
       }
       // Decrypt videos
       // const videos = doc.querySelectorAll("video");
