fcd0517bb8eea295986e3b43708a0bcabfdd5fa1
viccent55 <viccent55@gmail.com>
2025-10-10 15:11:51 +0700

    update compossable

--- composables/useSeo.ts
+++ composables/useSeo.ts
@@ -3,18 +3,25 @@ import { computed, type Ref } from "vue";
 export const useSeo = (
   title: Ref<string | undefined>,
   description: Ref<string | undefined>,
-  keyword: Ref<string | undefined>
+  keyword: Ref<string | undefined>,
+  author: Ref<string | undefined> = computed(() => "")
 ) => {
   useHead({
     title: computed(() => title?.value || "Default Website Title"),
     meta: [
+      {
+        name: "author",
+        content: computed(() => author?.value || "Default name."),
+      },
       {
         name: "description",
         content: computed(() => description?.value || "Default description."),
       },
       {
         name: "keywords",
-        content: computed(() => keyword?.value || "default, keywords, nuxt, vue"),
+        content: computed(
+          () => keyword?.value || "default, keywords, nuxt, vue"
+        ),
       },
     ],
   });
