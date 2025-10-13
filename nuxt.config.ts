ea4436e7db2d27bc820c061bca61cf10b47259e0
viccent55 <viccent55@gmail.com>
2025-10-13 10:35:29 +0700

    add route

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,7 +37,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE,
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://api.szy7.com/apiv1",
     },
   },
 
@@ -105,7 +105,7 @@ export default defineNuxtConfig({
       },
     },
     server: {
-       allowedHosts: true,
+      allowedHosts: true,
     },
   },
 
