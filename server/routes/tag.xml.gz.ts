55d36d553409d625c014a8116380ecc8e2de202e
viccent55 <viccent55@gmail.com>
2025-10-13 10:48:49 +0700

    update site map

--- server/routes/tag.xml.gz.ts
+++ server/routes/tag.xml.gz.ts
@@ -4,7 +4,7 @@ import { Readable } from 'stream'
 
 export default defineEventHandler(async (event) => {
   try {
-    const cleanName = 'tag' // This route is now static
+    const cleanName = 'tags' // This route is now static
 
     const config = useRuntimeConfig()
     // The apiBase should be correctly populated now after our nuxt.config.ts fix
