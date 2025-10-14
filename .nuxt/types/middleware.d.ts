91aa7f395dd8d0b9f809a1ad48b2cf00d8012cd5
viccent55 <viccent55@gmail.com>
2025-10-14 11:20:27 +0700

    Stop tracking .nuxt folder

--- .nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = "home"
-declare module 'nuxt/app' {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
--- .nuxt/types/nitro-middleware.d.ts
+++
@@ -1,6 +0,0 @@
-export type MiddlewareKey = "home"
-declare module 'nitropack' {
-  interface NitroRouteConfig {
-    appMiddleware?: MiddlewareKey | MiddlewareKey[] | Record<MiddlewareKey, boolean>
-  }
-}
