2862a3abbc32a44919f50a396eacadb74afbf984
viccent55 <viccent55@gmail.com>
2025-10-14 11:22:44 +0700

    added for capcitor

---
+++ capacitor.config.ts
@@ -0,0 +1,40 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const isDev = process.env.NODE_ENV !== "production";
+const LAN_IP = "http://192.168.100.101";
+const VITE_PORT = 3000;
+const LOCAL_URL = `${LAN_IP}:${VITE_PORT}`;
+const config: CapacitorConfig = {
+  appId: "redbook.xhltbes.app",
+  appName: "小红书成人版",
+  webDir: ".output/public", // ✅ Nuxt build folder (for production build)
+  server: isDev
+    ? {
+        // 👇 Use local Vite dev server for live reload
+        url: LOCAL_URL,
+        cleartext: true,
+      }
+    : {
+        // 👇 Use production site when in production
+        url: "https://redbook.xhltbes.com",
+        cleartext: true,
+      },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, // ms
+      launchAutoHide: true, // auto hide after web loads
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      splashFullScreen: true,
+      splashImmersive: true,
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+    },
+    StatusBar: {
+      style: "DARK",
+      backgroundColor: "#ffffffff",
+    },
+  },
+};
+
+export default config;
