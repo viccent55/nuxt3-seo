7b2fbe69fc1cb7b8739f4f6037df82768a721785
viccent55 <viccent55@gmail.com>
2025-10-20 18:17:12 +0700

    fix bug style

--- components/explore/PageContent.vue
+++ components/explore/PageContent.vue
@@ -189,7 +189,7 @@
 <style scoped lang="scss">
   .explore-wrapper {
     width: 100%; /* Default height for desktop */
-    max-width: calc(100vh - v-bind(heightOffset));
+    max-height: calc(100vh - v-bind(heightOffset));
     height: calc(100dvh - v-bind(heightOffset));
     display: flex;
     flex-direction: column;
--- pages/category/[id].vue
+++ pages/category/[id].vue
@@ -40,7 +40,8 @@
 <style scoped lang="scss">
   .explore-wrapper {
     width: 100%;
-    height: calc(100vh - v-bind(heightOffset));
+    max-height: calc(100vh - v-bind(heightOffset));
+    height: calc(100dvh - v-bind(heightOffset));
     display: flex;
     flex-direction: column;
     padding: 0 12px;
